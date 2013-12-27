fe725ef75ce54566d006d565d3800da816556b69
jzmaddock <john@johnmaddock.co.uk>
2013-12-27 17:26:13 +0000

    Refactor and better document code. Add support for no atomic int's, std::atomic, and no threads. Add performance test case.

--- boost/math/special_functions/bernoulli.hpp
+++ boost/math/special_functions/bernoulli.hpp
@@ -18,13 +18,7 @@
 #include <boost/math/special_functions/math_fwd.hpp>
 #include <boost/mpl/int.hpp>
 #include <boost/type_traits/is_convertible.hpp>
-#include <boost/detail/lightweight_mutex.hpp>
-//
-// We need Boost.Atomic, but on any platform that supports auto-linking we do
-// not need to link against a separate library:
-//
-#define BOOST_ATOMIC_NO_LIB
-#include <boost/atomic.hpp>
+#include <boost/math/special_functions/detail/bernoulli_details.hpp>
 
 namespace boost { namespace math { 
    
@@ -651,131 +645,6 @@ inline T unchecked_bernoulli_imp(std::size_t n, const mpl::int_<3>& )
    return bernoulli_data[n];
 }
 
-template <class T>
-struct bernoulli_overflow_variant
-{
-   static const unsigned value = 
-      (std::numeric_limits<T>::max_exponent == 128)
-      && (std::numeric_limits<T>::radix == 2) ? 1 :
-      (
-         (std::numeric_limits<T>::max_exponent == 1024)
-         && (std::numeric_limits<T>::radix == 2) ? 2 :
-         (
-            (std::numeric_limits<T>::max_exponent == 16384)
-            && (std::numeric_limits<T>::radix == 2) ? 3 : 0
-         )
-      );
-};
-
-template<class T>
-inline bool bernouli_impl_index_does_overflow(std::size_t, const mpl::int_<0>&)
-{
-   return false;
-}
-
-// There are certain cases for which the index n, when trying
-// to compute Bn, is known to overflow. In particular, when
-// using 32-bit float and 64-bit double (IEEE 754 conformant),
-// overflow will occur if the index exceeds the amount allowed
-// in the tables of Bn.
-template<class T>
-inline bool bernouli_impl_index_does_overflow(std::size_t n, const mpl::int_<1>&)
-{
-   // This corresponds to 4-byte float, IEEE 745 conformant.
-   return n >= max_bernoulli_index<1>::value * 2;
-}
-template<class T>
-inline bool bernouli_impl_index_does_overflow(std::size_t n, const mpl::int_<2>&)
-{
-   // This corresponds to 8-byte float, IEEE 745 conformant.
-   return n >= max_bernoulli_index<2>::value * 2;
-}
-template<class T>
-inline bool bernouli_impl_index_does_overflow(std::size_t n, const mpl::int_<3>&)
-{
-   // This corresponds to 16-byte float, IEEE 745 conformant.
-   return n >= max_bernoulli_index<3>::value * 2;
-}
-
-template<class T>
-inline bool bernouli_impl_index_does_overflow(std::size_t n)
-{
-   typedef mpl::int_<bernoulli_overflow_variant<T>::value> tag_type;
-   return bernouli_impl_index_does_overflow<T>(n, tag_type());
-}
-
-template<class T>
-bool bernouli_impl_index_might_overflow(std::size_t n)
-{
-   if(bernouli_impl_index_does_overflow<T>(n))
-   {
-      // If the index *does* overflow, then it also *might* overflow.
-      return true;
-   }
-
-   // Here, we use an asymptotic expansion of |Bn| from Luschny
-   // to estimate if a given index n for Bn *might* overflow.
-   static const T log_of_four_pi = log(boost::math::constants::two_pi<T>() * 2);
-   static const T two_pi_e       = boost::math::constants::two_pi<T>() * boost::math::constants::e<T>();
-   const float nf = static_cast<float>(n);
-   const T nx (nf);
-   const T nx2(nx * nx);
-   const T n_log_term     = (nx + boost::math::constants::half<T>()) * log(nx / two_pi_e);
-
-   const T approximate_log_of_bn =   log_of_four_pi
-      + n_log_term
-      + boost::math::constants::half<T>()
-      + (T(1) / (nx * 12))
-      - (T(1) / (nx2 * nx * 360))
-      + (T(1) / (nx2 * nx2 * nx * 1260))
-      + (n * boost::math::constants::ln_two<T>())
-      - log(nx)
-      + log(ldexp(T(1), (int)n) - 1);
-
-   return approximate_log_of_bn * 1.1 > boost::math::tools::log_max_value<T>();
-}
-
-template<class T>
-std::size_t possible_overflow_index()
-{
-   // we use binary search to determine a good approximation for an index that might overflow
-
-   std::size_t upper_limit = 100000;
-   std::size_t lower_limit = 1;
-
-   if(bernouli_impl_index_might_overflow<T>(upper_limit * 2) == 0)
-   {
-      return upper_limit;
-   }
-
-   while(upper_limit > (lower_limit + 4))
-   {
-      const int mid = (upper_limit + lower_limit) / 2;
-
-      if(bernouli_impl_index_might_overflow<T>(mid * 2) == 0)
-      {
-         lower_limit = mid;
-      }
-      else
-      {
-         upper_limit = mid;
-      }
-   }
-
-   return lower_limit;
-}
-
-template <class T>
-inline typename enable_if_c<std::numeric_limits<T>::is_specialized && (std::numeric_limits<T>::radix == 2), T>::type tangent_scale_factor()
-{
-   return ldexp(T(1), std::numeric_limits<T>::min_exponent + 5);
-}
-template <class T>
-inline typename disable_if_c<std::numeric_limits<T>::is_specialized && (std::numeric_limits<T>::radix == 2), T>::type tangent_scale_factor()
-{
-   return tools::min_value<T>() * 16;
-}
-
 
 template<class T, class Container, class Policy>
 inline void tangent(Container& tangent_numbers, std::size_t m, const Policy&)
@@ -896,89 +765,6 @@ OutputIterator bernoulli_number_imp(OutputIterator out, std::size_t start, std::
    return out;
 }
 
-//
-// Initializer: ensure all our constants are initialized prior to the first call of main:
-//
-template <class T, class Policy>
-struct bernoulli_initializer
-{
-   struct init
-   {
-      init()
-      {
-         boost::math::bernoulli_b2n<T>(max_bernoulli_b2n<T>::value + 1, Policy());
-      }
-      void force_instantiate()const{}
-   };
-   static const init initializer;
-   static void force_instantiate()
-   {
-      initializer.force_instantiate();
-   }
-};
-
-template <class T, class Policy>
-const typename bernoulli_initializer<T, Policy>::init bernoulli_initializer<T, Policy>::initializer;
-
-//
-// We need a type to use as an atomic counter:
-//
-#if BOOST_ATOMIC_INT_LOCK_FREE == 2
-typedef boost::atomic<int> atomic_counter_type;
-#elif BOOST_ATOMIC_SHORT_LOCK_FREE == 2
-typedef boost::atomic<short> atomic_counter_type;
-#elif BOOST_ATOMIC_LONG_LOCK_FREE == 2
-typedef boost::atomic<long> atomic_counter_type;
-#elif BOOST_ATOMIC_LLONG_LOCK_FREE == 2
-typedef boost::atomic<long long> atomic_counter_type;
-#else
-typedef boost::atomic<int> atomic_counter_type;
-#endif
-//
-// Very very simple vector class that will never allocate more than once, we could use
-// boost::container::static_vector here, but that allocates on the stack, which may well
-// cause issues for the amount of memory we want in the extreme case...
-//
-template <class T>
-struct fixed_vector : private std::allocator<T>
-{
-   typedef unsigned size_type;
-   typedef T* iterator;
-   typedef const T* const_iterator;
-   fixed_vector() : m_used(0)
-   { 
-      std::size_t overflow_limit = 100 + possible_overflow_index<T>();
-      m_capacity = (std::min)(overflow_limit, static_cast<std::size_t>(100000u));
-      m_data = this->allocate(m_capacity); 
-   }
-   ~fixed_vector()
-   {
-      for(unsigned i = 0; i < m_used; ++i)
-         this->destroy(&m_data[i]);
-      this->deallocate(m_data, m_capacity);
-   }
-   T& operator[](unsigned n) { BOOST_ASSERT(n < m_used); return m_data[n]; }
-   const T& operator[](unsigned n)const { BOOST_ASSERT(n < m_used); return m_data[n]; }
-   unsigned size()const { return m_used; }
-   unsigned size() { return m_used; }
-   void resize(unsigned n, const T& val)
-   {
-      if(n > m_capacity)
-         throw std::runtime_error("Exhausted storage for Bernoulli numbers.");
-      for(unsigned i = m_used; i < n; ++i)
-         new (m_data + i) T(val);
-      m_used = n;
-   }
-   void resize(unsigned n) { resize(n, T()); }
-   T* begin() { return m_data; }
-   T* end() { return m_data + m_used; }
-   T* begin()const { return m_data; }
-   T* end()const { return m_data + m_used; }
-private:
-   T* m_data;
-   unsigned m_used, m_capacity;
-};
-
 template <class T, class OutputIterator, class Policy>
 OutputIterator bernoulli_number_imp(OutputIterator out, std::size_t start, std::size_t n, const Policy& pol, const mpl::int_<0>& tag)
 {
@@ -992,13 +778,70 @@ OutputIterator bernoulli_number_imp(OutputIterator out, std::size_t start, std::
    //
    if(start + n <= max_bernoulli_b2n<T>::value)
       return out;
+   //
+   // There are basically 3 thread safety options:
+   //
+   // 1) There are no threads (BOOST_HAS_THREADS is not defined).
+   // 2) There are threads, but we do not have a true atomic integer type, 
+   //    in this case we just use a mutex to guard against race conditions.
+   // 3) There are threads, and we have an atomic integer: in this case we can
+   //    use the double-checked locking pattern to avoid thread synchronisation
+   //    when accessing values already in the cache.
+   //
+#if !defined(BOOST_HAS_THREADS)
+   //
+   // Single threaded code, very simple:
+   //
+   static fixed_vector<T> bn;
+   static std::size_t overflow_limit = (std::numeric_limits<std::size_t>::max)();
 
+   if(start + n >= bn.size())
+   {
+      std::size_t new_size = (std::max)((std::max)(start + n, std::size_t(bn.size() + 20)), std::size_t(50));
+      tangent_numbers_series<T>(bn, new_size, pol, &overflow_limit);
+   }
+
+   for(std::size_t i = (std::max)(max_bernoulli_b2n<T>::value + 1, start); i < start + n; ++i)
+   {
+      *out = (i >= overflow_limit) ? policies::raise_overflow_error<T>("boost::math::bernoulli<%1%>(std::size_t)", 0, pol) : bn[i];
+      ++out;
+   }
+#elif defined(BOOST_MATH_NO_ATOMIC_INT)
    //
    // Force this function to be called at program startup so all the static variables
    // get initailzed then (thread safety).
    //
    bernoulli_initializer<T, Policy>::force_instantiate();
+   //
+   // We need to grab a mutex every time we get here, for both readers and writers:
+   //
+   static fixed_vector<T> bn;
+   static boost::detail::lightweight_mutex m;
+   static std::size_t overflow_limit = (std::numeric_limits<std::size_t>::max)();
 
+   boost::detail::lightweight_mutex::scoped_lock l(m);
+   if(start + n >= bn.size())
+   {
+      std::size_t new_size = (std::max)((std::max)(start + n, std::size_t(bn.size() + 20)), std::size_t(50));
+      tangent_numbers_series<T>(bn, new_size, pol, &overflow_limit);
+   }
+
+   for(std::size_t i = (std::max)(max_bernoulli_b2n<T>::value + 1, start); i < start + n; ++i)
+   {
+      *out = (i >= overflow_limit) ? policies::raise_overflow_error<T>("boost::math::bernoulli<%1%>(std::size_t)", 0, pol) : bn[i];
+      ++out;
+   }
+
+#else
+   //
+   // Force this function to be called at program startup so all the static variables
+   // get initailzed then (thread safety).
+   //
+   bernoulli_initializer<T, Policy>::force_instantiate();
+   //
+   // Double-checked locking pattern, lets us access cached already cached values
+   // without locking:
+   //
    static fixed_vector<T> bn;
    static atomic_counter_type counter(0);
    static boost::detail::lightweight_mutex m;
@@ -1022,12 +865,13 @@ OutputIterator bernoulli_number_imp(OutputIterator out, std::size_t start, std::
       }
    }
 
-
    for(std::size_t i = (std::max)(max_bernoulli_b2n<T>::value + 1, start); i < start + n; ++i)
    {
       *out = (i >= overflow_limit) ? policies::raise_overflow_error<T>("boost::math::bernoulli<%1%>(std::size_t)", 0, pol) : bn[i];
       ++out;
    }
+
+#endif
    return out;
 }
 
